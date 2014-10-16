d8f5730801f7cd315e97a6588e57e59fa5711426
chante29 <davidmarchantecano@gmail.com>
2014-10-16 19:44:22 +0000

    Realizado ejercicio Patrón Singleton

---
+++ src/main/java/pd/singleton/factory/FactoryReferences.java
@@ -0,0 +1,39 @@
+package pd.singleton.factory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class FactoryReferences {
+	private static FactoryReferences factoryReferences = null;
+	private Map<String, Integer> references;
+
+    private int reference;
+
+    private FactoryReferences() {
+        this.references = new HashMap<>();
+        this.reference = 0;
+    }
+    
+    public static FactoryReferences getFactory(){
+    	if (FactoryReferences.factoryReferences == null) {
+    		FactoryReferences.factoryReferences = new FactoryReferences();
+        }
+        return FactoryReferences.factoryReferences;
+    }
+
+    public int getReference(String key) {
+        Integer result = this.references.get(key);
+        if (result == null) {
+            this.references.put(key, this.reference);
+            result = this.reference;
+            reference++;
+        }
+        return result;
+    }
+
+    public void removeReference(String key) {
+        this.references.remove(key);
+    }
+
+}
